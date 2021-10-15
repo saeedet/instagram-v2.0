e359472f0f8635ccfd237917dde652319a861c2d
Saeed <saeed_et90@yahoo.com>
2021-10-15 19:38:54 +1100

    envs corrected

--- Firebase/firebase.ts
+++ Firebase/firebase.ts
@@ -5,22 +5,13 @@ import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDL7P4VB3ZoROi9Emr-fXDPDZ8gddelgtA",
-//   authDomain: "instagram-3fc03.firebaseapp.com",
-//   projectId: "instagram-3fc03",
-//   storageBucket: "instagram-3fc03.appspot.com",
-//   messagingSenderId: "788658813978",
-//   appId: "1:788658813978:web:ad1a46a1774f32391b65a3",
-// };
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
